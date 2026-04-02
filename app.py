cd5628fc14d526dc7ac747f1f0e70a7bf950950e
k0k4developer <oskarcermeno1@gmail.com>
2026-04-02 23:49:01 +0000

    Updating to f string

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import mysql.connector
 from mysql.connector import Error
 from fastapi import FastAPI, HTTPException, Depends
-from typing import Annotated
+from typing import Annotated, Any
 
 app=FastAPI(title='FastAPI CRUD API')
+
+def get_db():
+    host='10.10.10.26'
+    user='user'
+    password='katty2023'
+    database='vuln_db'
+    db=create_connection(host, user, password, database)
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency=Annotated[Any, Depends(get_db)]
 
 def create_connection(host, user, password, database):
     try:
@@ -18,11 +31,11 @@ def create_connection(host, user, password, database):
         print(f'Error de conexion: {e}')
 
 @app.post('/users')
-def insert_students(db:db_dependency, id_estudiante, nombre_estudiante, apellido_estudiante, id_grado, seccion):
+def insert_students(db:db_dependency, id_estudiante:int, nombre_estudiante:str, apellido_estudiante:str, id_grado:int, seccion:str):
     try:
-        db_conn=db.cursor()
-        db_conn.execute('INSERT INTO estudiantes(id_estudiante, nombre_estudiante, apellido_estudiante, id_grado, seccion) VALUES (' + id_estudiante + nombre_estudiante+ apellido_estudiante+ id_grado + seccion + ');' )
-        db_conn.commit()
+        cursor=db.cursor()
+        cursor.execute(f"INSERT INTO estudiantes(id_estudiante, nombre_estudiante, apellido_estudiante, id_grado, seccion) VALUES ( '{id_estudiante}', '{nombre_estudiante}', '{apellido_estudiante}', '{id_grado}', '{ seccion}' )" )
+        db.commit()
         return {'status': 'query inserted correctly'}
     except Error as e:
         return {'Error insertando valores:' : str(e)}
@@ -30,22 +43,11 @@ def insert_students(db:db_dependency, id_estudiante, nombre_estudiante, apellido
 @app.get('/courses')
 def show_courses(db:db_dependency):
     try:
-        db_conn=db.cursor()
-        db_conn.execute('SELECT nombre_curso, id_curso AS codigo_curso FROM cursos')
-        rows= db_conn.fetchall()
+        cursor=db.cursor()
+        cursor.execute('SELECT nombre_curso, id_curso AS codigo_curso FROM cursos')
+        rows= cursor.fetchall()
         return{'cursos:' : rows}
     except Error as e:
         return {'error': str(e)}
 
-def get_db():
-    host='10.10.10.26'
-    user='user'
-    password='katty2023'
-    database='vuln_db'
-    db=create_connection(host, user, password, database)
-    try:
-        yield db
-    finally:
-        db.close()
 
-db_dependency=Annotated[Session, Depends(get_db)]
